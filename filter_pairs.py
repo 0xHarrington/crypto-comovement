3eea675489f7608be0e7f453fa5b835c0cbd172a
Matt Harrington <harringtonswim@gmail.com>
2021-03-10 17:37:34 -0500

    Initial commit, migrating from the private repo containing my course materials

---
+++ filter_pairs.py
@@ -0,0 +1,31 @@
+import sys
+import os
+from subsets import *
+
+def main(directory):
+    subset = top_50 # CHANGE IF NECESSARY
+    for dirname, _, filenames in os.walk(directory):
+        for filename in filenames:
+
+            #  debugging
+            #  print(filename)
+
+            # Grab coin names
+            coins, _ = filename.split('.')
+            c1, c2 = coins.split('-')
+
+            # Handle blacklist
+            if c1 in blacklist or c2 in blacklist:
+                os.remove(dirname + '/' + filename)
+                continue
+
+            # print target for now
+            if (c1 in subset and c2 in stable) or (c2 in subset and c1 in stable):
+                print(dirname + "/" + filename)
+            else:
+                os.remove(dirname + '/' + filename)
+
+print("Did you pass the directory as an arg?")
+print(sys.argv)
+#  pass the command line arg directory to the walker
+main(sys.argv[1])
---
+++ subsets.py
@@ -0,0 +1,20 @@
+#  Initialize coin subsets
+top_50 = ["BTC", "ETH", "USDT", "XRP", "BCH", "BNB", "LINK", "DOT", "ADA", "LTC", "BSV", "CRO", "USDC", "EOS", 
+          "XMR", "TRX", "XTZ", "XLM", "NEO", "WBTC", "LEO", "ATOM", "XEM", "HT", "DAI", "FIL", "BUSD", "MIOTA", 
+          "ZEC", "VET", "THETA", "DASH", "LEND", "ETC", "MKR", "UNI", "OMG", "UMA", "ONT", "YFI", "ABBC", "SNX", 
+          "OKB", "FTT", "BTT", "TUSD", "ALGO", "DGB", "DOGE", "REN"] 
+          # 45 non-stable coins
+          # 199 pairs
+
+top_30 = ["BTC", "ETH", "USDT", "XRP", "BCH", "BNB", "LINK", "DOT", "ADA", "LTC", "BSV", "CRO", "USDC", "EOS", 
+          "XMR", "TRX", "XTZ", "XLM", "NEO", "WBTC", "LEO", "ATOM", "XEM", "HT", "DAI", "FIL", "BUSD", "MIOTA", 
+          "ZEC", "VET"] 
+          # 26 non-stable coins
+          # 105 pairs within this group, 21 with USDT
+
+top_10 = ["BTC", "ETH", "USDT", "XRP", "BCH", "BNB", "LINK", "DOT", "ADA", "LTC"] # 27 
+
+stable = ["USDT", "TUSD", 'DAI', 'USDC', 'PAX', "BITUSD", "EOSDT", "GUSD", "BGBP", "USDS", "RSV", "BUSD"]
+
+blacklist = ["XRP"]
+
